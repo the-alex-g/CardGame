ed0e1267513ba068e7802aff11a0cd06ac9c7f20
the-alex-g <alex.gestwicki@gmail.com>
2023-06-12 09:12:03 -0400

    Add being able to click on Target-class objects

--- project/shared/target.gd
+++ project/shared/target.gd
@@ -6,10 +6,22 @@ signal died
 @export var team_index := 0
 
 var is_dead := false : set = _set_is_dead
+var _focused := false
 
 
 func _ready()->void:
+	input_pickable = true
+	mouse_entered.connect(Callable(self, "_on_mouse_entered"))
+	mouse_exited.connect(Callable(self, "_on_mouse_exited"))
 	TargetTracker.add_target(self)
+
+
+func _input(event:InputEvent)->void:
+	if not _focused:
+		return
+	if event is InputEventMouseButton:
+		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+			_resolve_click()
 
 
 func damage(_attack_array:Array)->void:
@@ -22,3 +34,15 @@ func _set_is_dead(value:bool)->void:
 		died.emit()
 		queue_free()
 	is_dead = value
+
+
+func _resolve_click()->void:
+	print("click")
+
+
+func _on_mouse_entered()->void:
+	_focused = true
+
+
+func _on_mouse_exited():
+	_focused = false
